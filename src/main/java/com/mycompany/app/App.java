3578fbcaa84a9f151575bf924a9a87bcc9bbb89c
Joaquin Camara <jcamara@NSL-JCAMARAs-MacBook-Pro.local>
2020-04-02 14:12:17 -0600

    This commit is to add the possibilty to print the value of an URL param

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,19 @@
 package com.mycompany.app;
 
-//import static com.mycompany.app.ApiUtils.splitQuery;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
-//import java.util.List;
-//import java.util.Map;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import com.sun.net.httpserver.HttpServer;
 
@@ -15,19 +22,17 @@ import com.sun.net.httpserver.HttpServer;
  *
  */
 public class App {
+
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/api/capitan", (exchange -> {
-            String respText = "Hello capitan!";
 
-            if ("POST".equals(exchange.getRequestMethod())) {
-                // Map<String, List<String>> params =
-                // splitQuery(exchange.getRequestURI().getRawQuery());
-                // String noNameText = "Anonymous";
-                // String name = params.getOrDefault("name",
-                // List.of(noNameText)).stream().findFirst().orElse(noNameText);
-                // String respText = String.format("Hello capitan %s!", name);
+            if ("GET".equals(exchange.getRequestMethod())) {
+                Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
+                String noNameText = "Anonymous";
+                String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+                String respText = String.format("Ahoy capitan %s!", name);
                 exchange.sendResponseHeaders(200, respText.getBytes().length);
                 OutputStream output = exchange.getResponseBody();
                 output.write(respText.getBytes());
@@ -39,5 +44,23 @@ public class App {
         }));
         server.setExecutor(null); // creates a default executor
         server.start();
+    }
+
+    public static Map<String, List<String>> splitQuery(String query) {
+        if (query == null || "".equals(query)) {
+            return Collections.emptyMap();
+        }
+
+        return Pattern.compile("&").splitAsStream(query).map(s -> Arrays.copyOf(s.split("="), 2))
+                .collect(groupingBy(s -> decode(s[0]), mapping(s -> decode(s[1]), toList())));
+
+    }
+
+    private static String decode(final String encoded) {
+        try {
+            return encoded == null ? null : URLDecoder.decode(encoded, "UTF-8");
+        } catch (final UnsupportedEncodingException e) {
+            throw new RuntimeException("UTF-8 is a required encoding", e);
+        }
     }
 }
